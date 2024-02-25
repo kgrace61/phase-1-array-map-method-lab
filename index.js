6a9a6e44c5400b583ac59a02070e0b12f6dc1ebd
Kristen <kgrace61@gmail.com>
2024-02-24 22:39:02 -0600

    completes lab

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const titleCasedTutor = tutorials.map(tutorial => {
+    const words = tutorial.split(" ");
+    const titleCaseWords = words.map(word => {
+      return word.charAt(0).toUpperCase()+ word.slice(1);
+    });
+    return titleCaseWords.join(" ")
+    });
+    return titleCasedTutor;
+  }
+ 
+// iterate through the tutorials array
+//modify the name of each tutorial
+//also need to access and mofidy each individual word
+
+//how can we iterate through words in a string?
+//possible: use split() ? use map() advantages 
+//use arrow function
+
+//can we execute an interation inside an iteration?
+//how can we capitalize just the first letter in a word?
+// startsWith() to.Upper.Case
